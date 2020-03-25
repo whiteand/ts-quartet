7c2f7342b9a754032d8f6500b09e4eba1ee2d635
AndrewBeletskiy <Andrew.Beletskiy@gdxtec.com>
2020-03-25 23:52:39 +0200

    dded several tests for object compilation

--- src/__tests__/object.test.ts
+++ src/__tests__/object.test.ts
@@ -1,5 +1,6 @@
 import { v } from "../index";
 import { snapshot, puretables } from "./utils";
+import { primitives, funcSchemaWithPrepare } from "./mocks";
 
 describe("v(object)", () => {
   test("00. v({})", () => {
@@ -9,37 +10,126 @@ describe("v(object)", () => {
     puretables(validator, [1, true, {}, [], "", false], [null, undefined]);
   });
   test("01. v({ a: null })", () => {
-    // TODO: Add it!
+    const validator = v({ a: null });
+    expect(validator.pure).toBe(true);
+    snapshot(validator);
+    puretables(
+      validator,
+      [{ a: null }, { a: null, b: 42 }, Object.assign([], { a: null })],
+      [...primitives, {}, { a: undefined }, { a: "null" }]
+    );
   });
   test("02. v({ a: undefined })", () => {
-    // TODO: Add it!
+    const validator = v({ a: undefined });
+    expect(validator.pure).toBe(true);
+    snapshot(validator);
+    puretables(
+      validator,
+      [
+        { a: undefined },
+        { a: undefined, b: 42 },
+        Object.assign([], { a: undefined })
+      ],
+      [null, undefined, { a: null }, { a: "null" }]
+    );
   });
   test("03. v({ a: NaN })", () => {
-    // TODO: Add it!
+    const validator = v({ a: NaN });
+    expect(validator.pure).toBe(true);
+    snapshot(validator);
+    puretables(
+      validator,
+      [{ a: NaN }, { a: NaN, b: 42 }, Object.assign([], { a: NaN })],
+      [...primitives, {}, { a: null }, { a: "null" }]
+    );
   });
   test("04. v({ a: 42 })", () => {
-    // TODO: Add it!
+    const validator = v({ a: 42 });
+    expect(validator.pure).toBe(true);
+    snapshot(validator);
+    puretables(
+      validator,
+      [{ a: 42 }, { a: 42, b: 42 }, Object.assign([], { a: 42 })],
+      [...primitives, {}, { a: null }, { a: "null" }]
+    );
   });
   test('05. v({ a: "true" })', () => {
-    // TODO: Add it!
+    const validator = v({ a: "true" });
+    expect(validator.pure).toBe(true);
+    snapshot(validator);
+    puretables(
+      validator,
+      [{ a: "true" }, { a: "true", b: "" }, Object.assign([], { a: "true" })],
+      [...primitives, {}, { a: null }, { a: true }, { a: "null" }]
+    );
   });
   test('06. v({ a: "false" })', () => {
-    // TODO: Add it!
+    const validator = v({ a: "false" });
+    expect(validator.pure).toBe(true);
+    snapshot(validator);
+    puretables(
+      validator,
+      [
+        { a: "false" },
+        { a: "false", b: "" },
+        Object.assign([], { a: "false" })
+      ],
+      [...primitives, {}, { a: null }, { a: false }, { a: "null" }]
+    );
   });
   test('07. v({ a: "test" })', () => {
-    // TODO: Add it!
+    const validator = v({ a: "test" });
+    expect(validator.pure).toBe(true);
+    snapshot(validator);
+    puretables(
+      validator,
+      [{ a: "test" }, { a: "test", b: "" }, Object.assign([], { a: "test" })],
+      [...primitives, {}, { a: null }, { a: false }, { a: "null" }]
+    );
   });
   test('08. v({ a: Symbol.for("test") })', () => {
-    // TODO: Add it!
+    const validator = v({ a: Symbol.for("test") });
+    expect(validator.pure).toBe(true);
+    snapshot(validator);
+    puretables(
+      validator,
+      [
+        { a: Symbol.for("test") },
+        { a: Symbol.for("test"), b: "" },
+        Object.assign([], { a: Symbol.for("test") })
+      ],
+      [...primitives, {}, { a: null }, { a: false }, { a: "null" }]
+    );
   });
   test("09. v({ a: true })", () => {
-    // TODO: Add it!
+    const validator = v({ a: true });
+    expect(validator.pure).toBe(true);
+    snapshot(validator);
+    puretables(
+      validator,
+      [{ a: true }, { a: true, b: "" }, Object.assign([], { a: true })],
+      [...primitives, {}, { a: null }, { a: false }, { a: "null" }]
+    );
   });
   test("10. v({ a: false })", () => {
-    // TODO: Add it!
+    const validator = v({ a: false });
+    expect(validator.pure).toBe(true);
+    snapshot(validator);
+    puretables(
+      validator,
+      [{ a: false }, { a: false, b: "" }, Object.assign([], { a: false })],
+      [...primitives, {}, { a: null }, { a: true }, { a: "null" }]
+    );
   });
   test("11. v({ a: funcWithPrepare })", () => {
-    // TODO: Add it!
+    const validator = v({ a: funcSchemaWithPrepare });
+    expect(validator.pure).toBe(true);
+    snapshot(validator);
+    puretables(
+      validator,
+      [{ a: 2 }, { a: 4, b: 1 }],
+      [...primitives, { a: 1 }, {}]
+    );
   });
   test("12. v({ a: funcWithoutPrepare })", () => {
     // TODO: Add it!
