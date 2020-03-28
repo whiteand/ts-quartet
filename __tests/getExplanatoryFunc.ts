8c3a4cb80ffae8d2872a7da2f2748ca28d0897bc
AndrewBeletskiy <Andrew.Beletskiy@gdxtec.com>
2020-03-28 13:14:36 +0200

    Removed old tests and testcases.md

--- __tests/getExplanatoryFunc.ts
+++
@@ -1,9 +0,0 @@
-export const getExplanatoryFunc = (
-  validValue: string,
-  explanation: string
-) => () => ({
-  check: (v: any) => `${v} === ${JSON.stringify(validValue)}`,
-  not: (v: any) => `${v} !== ${JSON.stringify(validValue)}`,
-  handleError: (v: any, ctx: any) =>
-    `${ctx}.explanations.push(${JSON.stringify(explanation)})`
-});
