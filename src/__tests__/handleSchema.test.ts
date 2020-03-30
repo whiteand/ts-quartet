d4ca07e62bd12104e2cbb2b91ffbe64c60838823
AndrewBeletskiy <Andrew.Beletskiy@gdxtec.com>
2020-03-30 11:38:34 +0300

    Fixed handlSchema test

--- src/__tests__/handleSchema.test.ts
+++ src/__tests__/handleSchema.test.ts
@@ -5,127 +5,135 @@ import { funcSchema } from "./mocks";
 describe("handleSchema", () => {
   test("switches in appropriate way", () => {
     const handle = handleSchema<any>({
+      and: andSchemas => andSchemas,
       constant: constant => ["constant", constant],
       function: func => ["func", typeof func],
       object: obj => ["obj", obj],
       objectRest: withRest => ["objWithRest", withRest],
       variant: schemas => ["variants", schemas]
     });
-    expect(handle(funcSchema)).toMatchInlineSnapshot(`
+    expect(handle(v.and(1, 2))).toMatchInlineSnapshot(`
       Array [
-        "func",
-        "function",
+        "__quartet/and__",
+        1,
+        2,
       ]
     `);
+    expect(handle(funcSchema)).toMatchInlineSnapshot(`
+            Array [
+              "func",
+              "function",
+            ]
+        `);
     expect(handle(undefined)).toMatchInlineSnapshot(`
-            Array [
-              "constant",
-              undefined,
-            ]
-        `);
+                  Array [
+                    "constant",
+                    undefined,
+                  ]
+            `);
     expect(handle(null)).toMatchInlineSnapshot(`
-            Array [
-              "constant",
-              null,
-            ]
-        `);
+                  Array [
+                    "constant",
+                    null,
+                  ]
+            `);
     expect(handle(true)).toMatchInlineSnapshot(`
-            Array [
-              "constant",
-              true,
-            ]
-        `);
+                  Array [
+                    "constant",
+                    true,
+                  ]
+            `);
     expect(handle(false)).toMatchInlineSnapshot(`
-            Array [
-              "constant",
-              false,
-            ]
-        `);
+                  Array [
+                    "constant",
+                    false,
+                  ]
+            `);
     expect(handle(0)).toMatchInlineSnapshot(`
-            Array [
-              "constant",
-              0,
-            ]
-        `);
+                  Array [
+                    "constant",
+                    0,
+                  ]
+            `);
     expect(handle(1)).toMatchInlineSnapshot(`
-            Array [
-              "constant",
-              1,
-            ]
-        `);
+                  Array [
+                    "constant",
+                    1,
+                  ]
+            `);
     expect(handle(NaN)).toMatchInlineSnapshot(`
-            Array [
-              "constant",
-              NaN,
-            ]
-        `);
+                  Array [
+                    "constant",
+                    NaN,
+                  ]
+            `);
     expect(handle(Infinity)).toMatchInlineSnapshot(`
-            Array [
-              "constant",
-              Infinity,
-            ]
-        `);
+                  Array [
+                    "constant",
+                    Infinity,
+                  ]
+            `);
     expect(handle(-Infinity)).toMatchInlineSnapshot(`
-            Array [
-              "constant",
-              -Infinity,
-            ]
-        `);
+                  Array [
+                    "constant",
+                    -Infinity,
+                  ]
+            `);
     expect(handle("")).toMatchInlineSnapshot(`
-            Array [
-              "constant",
-              "",
-            ]
-        `);
+                  Array [
+                    "constant",
+                    "",
+                  ]
+            `);
     expect(handle("Andrew")).toMatchInlineSnapshot(`
-            Array [
-              "constant",
-              "Andrew",
-            ]
-        `);
+                  Array [
+                    "constant",
+                    "Andrew",
+                  ]
+            `);
     expect(handle(Symbol.for("test"))).toMatchInlineSnapshot(`
-            Array [
-              "constant",
-              Symbol(test),
-            ]
-        `);
+                  Array [
+                    "constant",
+                    Symbol(test),
+                  ]
+            `);
     expect(handle({ a: 1 })).toMatchInlineSnapshot(`
-            Array [
-              "obj",
-              Object {
-                "a": 1,
-              },
-            ]
-        `);
+                  Array [
+                    "obj",
+                    Object {
+                      "a": 1,
+                    },
+                  ]
+            `);
     expect(handle({ a: 1, [v.rest]: 2 })).toMatchInlineSnapshot(`
-            Array [
-              "objWithRest",
-              Object {
-                "__quartet/rest__": 2,
-                "a": 1,
-              },
-            ]
-        `);
+                  Array [
+                    "objWithRest",
+                    Object {
+                      "__quartet/rest__": 2,
+                      "a": 1,
+                    },
+                  ]
+            `);
     expect(handle({ a: 1, [v.rest]: 2, [v.restOmit]: ["a"] }))
       .toMatchInlineSnapshot(`
-            Array [
-              "objWithRest",
-              Object {
-                "__quartet/rest-omit__": Array [
-                  "a",
-                ],
-                "__quartet/rest__": 2,
-                "a": 1,
-              },
-            ]
-        `);
+                  Array [
+                    "objWithRest",
+                    Object {
+                      "__quartet/rest-omit__": Array [
+                        "a",
+                      ],
+                      "__quartet/rest__": 2,
+                      "a": 1,
+                    },
+                  ]
+            `);
     expect(handle({ a: 1, [v.restOmit]: ["a"] })).toMatchInlineSnapshot(`
-            Array [
-              "obj",
-              Object {
-                "a": 1,
-              },
-            ]
-        `);
+                  Array [
+                    "obj",
+                    Object {
+                      "a": 1,
+                    },
+                  ]
+            `);
   });
 });
