84df2af4f2e4f92c0ac20d09d808e4a27e88a896
Andrew Beletskiy <andrewbeletskiy@gmail.com>
2020-08-04 09:06:45 +0300

    Added tests for e.array

--- src/tests/e.array.test.ts
+++ src/tests/e.array.test.ts
@@ -1,12 +1,50 @@
-import { e as v } from "..";
-import { testValidatorImpure } from "./testValidatorImpure";
+import { e as v } from '..'
+import { testValidatorWithExplanations } from './testValidatorWithExplanations'
+import { ExplanationSchemaType } from '../explanations'
 
-describe("v.array", () => {
-  test("v.array", () => {
-    testValidatorImpure(
+describe('v.array', () => {
+  test('v.array', () => {
+    testValidatorWithExplanations(
       v(v.array),
-      [[], [1, 2, "3"]],
-      [{}, { length: 10 }, "Andrew"]
-    );
-  });
-});
+      [[], [1, 2, '3']],
+      [
+        [
+          {},
+          [
+            {
+              value: {},
+              path: [],
+              schema: {
+                type: ExplanationSchemaType.Array,
+              },
+            },
+          ],
+        ],
+        [
+          { length: 10 },
+          [
+            {
+              value: { length: 10 },
+              path: [],
+              schema: {
+                type: ExplanationSchemaType.Array,
+              },
+            },
+          ],
+        ],
+        [
+          'Andrew',
+          [
+            {
+              value: 'Andrew',
+              path: [],
+              schema: {
+                type: ExplanationSchemaType.Array,
+              },
+            },
+          ],
+        ],
+      ],
+    )
+  })
+})
